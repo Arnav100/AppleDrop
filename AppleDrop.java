8839ab2aaf400ab23eeace3a3a30ed64ad314477
Arnav100 <parashararnav2@gmail.com>
2019-10-30 13:05:54 -0400

    save

---
+++ AppleDrop.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class AppleDrop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AppleDrop
+{
+    /*Player1 = mouse 
+     * red apples - slow, left-click + points for p1
+     * green apples - faster (variable speed, length held) left-click + more pts for p1
+     * brown rotten apples - medium speed, right-click - remove pts for p2
+     * 
+     * Player2 = keyboard
+     * 
+     * left/right + <space> swatting away bad apples
+     * 
+     * 
+     * Ideas: 
+     * 
+     * All speeds should be written as relative to the screen size
+     * 
+     * 1 point for red and then a point based on a ratio of points to speed
+     * 
+     * Basket should wrap around to the other side of the screen if it hits one end
+     * 
+     * 1 minute rounds, number can be adjusted later
+     * 
+     * Mac:
+     * 
+     * setExtendedState java
+    */
+}
